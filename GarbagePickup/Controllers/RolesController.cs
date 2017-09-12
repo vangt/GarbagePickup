7b04d5b1c0ca26a7160ff15bce5d6894ce2be048
vangt <vangt86@gmail.com>
2017-09-12 08:39:32 -0500

    Added Customer, Employee, and Admin views.  Added code to redirect to those views.

--- GarbagePickup/Controllers/RolesController.cs
+++ GarbagePickup/Controllers/RolesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GarbagePickup.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,45 @@ namespace GarbagePickup.Controllers
     {
         // GET: Roles
         public ActionResult Index()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            if (User.Identity.IsAuthenticated)
+            {
+                if (User.IsInRole("Customer"))
+                {
+                    return RedirectToAction("Customer");
+                }
+                else if (User.IsInRole("Employee"))
+                {
+                    return RedirectToAction("Employee");
+                }
+                else if (User.IsInRole("Admin"))
+                {
+                    return RedirectToAction("Admin");
+                }
+                else
+                {
+                    return View("Index", "Home");
+                }
+            }
+            else
+            {
+                return View("Index", "Home");
+            }
+        }
+
+        public ActionResult Customer()
+        {
+            return View();
+        }
+
+        public ActionResult Employee()
+        {
+            return View();
+        }
+
+        public ActionResult Admin()
         {
             return View();
         }
